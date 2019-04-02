f4584568257a2cc3fa541179a16aa6c405df88df
Ashan.97 <ashankaushalya952@gmail.com>
2019-04-02 19:22:03 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,73 @@
+<?php session_start(); ?>
+<?php 
+    require_once('inc/connection.php');
+?>
+<?php 
+    require_once('inc/functions.php');
+?>
+<?php
+
+    // check for form submission
+    if (isset($_POST['submit'])) {
+
+            $errors = array();
+
+            // check if the username and password has been entered.
+            if(!isset($_POST['email']) || strlen(trim($_POST['email'])) < 1){
+                $errors[] = 'Username is missing / invalid.';
+            }
+            if(!isset($_POST['password']) || strlen(trim($_POST['password'])) < 1){
+                $errors[] = 'password is missing / invalid.';
+            }
+            // check if there are any errors in the form
+            if(empty($errors)) {
+
+                // save username and password into variables
+                $email = mysqli_real_escape_string($connection,$_POST['email']);
+                $password = mysqli_real_escape_string($connection,$_POST['password']);
+                $hashed_password = sha1($password);
+
+                // prepare dat
+/*                 abase query
+ */                $query = "SELECT * FROM user
+                            WHERE email = '{$email}'
+                            AND password = '{$hashed_password}'
+                            LIMIT 1";
+   
+                 $result_set = mysqli_query($connection, $query);
+
+                 verify_query($result_set); 
+                 
+                    //query succesfull.
+                    if (mysqli_num_rows($result_set) == 1) {
+                        //valid user found.
+                        $user = mysqli_fetch_assoc($result_set);
+                        $_SESSION['user_id'] = $user['id'];
+                        $_SESSION['first_name'] = $user['first_name'];
+                        
+                        // updation last login
+                        $query = "UPDATE user SET last_login = NOW()";
+                        $query .= "WHERE id = {$_SESSION['user_id']} LIMIT 1";
+
+                        $result_set = mysqli_query($connection, $query);
+
+                        verify_query($result_set); 
+
+
+
+                        header("Location: users.php");
+                    }else {
+                        //username or password invalid.
+                        $errors[] = 'Invalid username / password.';
+                    }
+
+
+
+            }
+        
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
