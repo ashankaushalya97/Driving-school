465a249c16e68a0ae5f3d68475eb6ab584220bd3
Badhrajith-Pathirana <badhrajith.pathirana@gmail.com>
2019-04-03 15:54:59 +0530

    Changes repo update string

---
+++ api/v1/service/impl/CourseServiceImpl.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: beempz
+ * Date: 4/3/19
+ * Time: 3:40 PM
+ */
+
+class CourseServiceImpl implements CourseService
+{
+
+    function addCourse($course_id, $course_name, $fee)
+    {
+
+    }
+
+    function changeCourseDetails($course_id, $course_name, $fee)
+    {
+        // TODO: Implement changeCourseDetails() method.
+    }
+
+    function getCourseUsers($course_id)
+    {
+        // TODO: Implement getCourseUsers() method.
+    }
+}
